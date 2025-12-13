1d5e707833ddf1c0a84616aed74b44c1020e42a5
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-12-14 04:43:55 +0900

    feat: add @lycorp-jp/tappy to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const config: NextConfig = {
-  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
+  serverExternalPackages: [
+    "puppeteer-core",
+    "@sparticuz/chromium-min",
+    "@lycorp-jp/tappy",
+  ],
 };
 
 export default config;
