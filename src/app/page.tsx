4efd0a21d5bb05cb59df52f64e0aa47f9e44c68a
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-12-14 01:25:18 +0900

    feat: add global background and theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">tappy-valley</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline h-screen">tappy-valley</h1>
+  );
 }
